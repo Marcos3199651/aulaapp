d24854a4590a5b995f46340aef69d8fa55ce50ed
Marcos <marcosviniciusoliveira2006gmail.com>
2026-09-21 20:24:20 -0300

    ta salvo

--- server.js
+++ server.js
@@ -2,14 +2,26 @@ const express = require('express');
 const app = express()
 const port = 3000
 
+const books = [
+    {id: 1, title: 'Harry Potter', author: 'J.K Rowling'},
+    {id: 2, title: 'Harry Potter e a camara secreta', author: 'J.K Rowling'}
+]
+
 app.get('/', (req, res) => {
-  res.send('Hello World TURMA!')
+  res.send('API-BOOL está funcionando!')
 })
 
-app.get('/turma', (req, res) => {
-  res.send('Me acompanhe nesse teste TURMA!')
+app.get('/books', (req, res) => {
+  res.json(books)
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get('/books/:id', (req, res) => {
+    const bookId = parseInt(req.params.id)
+    const book = books.find(b => b.id === bookId)
+    res.json(book)
+    
+} )
+
+app.listen(port, ()=> {
+    console.log(`TA funcionando`)
 })
